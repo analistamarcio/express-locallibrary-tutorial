c531f7ef8ea419d7adec22bbfff9de8dd23d5f51
Marcio Ferreira <analistamarcio@hotmail.com>
2021-07-22 10:13:40 -0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = process.env.MONGO_SERVER;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
